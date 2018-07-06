d740afa47d9c6f1e69ac535cfe8563fbfb928014
Laura Woelbeling <s0548106@htw-berlin.de>
2018-07-06 15:32:21 +0200

    fixed testLifeCycle()

--- src/de/sb/tournament/persistence/MatchEntityTest.java
+++ src/de/sb/tournament/persistence/MatchEntityTest.java
@@ -22,13 +22,18 @@ public class MatchEntityTest extends EntityTest{
 		try {
 		
 			validator = vf.getValidator();
-			Match entity = new Match( ) ;
+			Competitor left = new Competitor("GER", "Germany", null);
+			Competitor right = new Competitor("FRA", "France", null);
+			Group g = new Group();
+			g.setAlias("A");
+			Match entity = new Match(g, left, right) ;
 			entity.setLeftParentRank((byte) 0);
 			entity.setRightParentRank((byte) 0);
 			entity.setRightScore((short) 0);
 			entity.setLeftScore((short) 0);
 			
 			Set<ConstraintViolation<Match>> constraintViolations = validator.validate(entity);
+			System.out.println(constraintViolations);
 			assertEquals( 0, constraintViolations.size() );
 		} finally {
 			vf.close();
@@ -38,27 +43,33 @@ public class MatchEntityTest extends EntityTest{
   
 	@Test
 	public void testLifeCycle() {
+		
 		EntityManager em = super.getEntityManagerFactory().createEntityManager();
 		Set<Long> wb = super.getWasteBasket();
 		
-		em.getTransaction().begin();
-		Match entity = new Match() ;
-		em.persist(entity);
-		em.getTransaction().commit();
 		try {
-			EntityTest obj = em.getReference(EntityTest.class, entity.getIdentity());
-		   
-		} catch (EntityNotFoundException e) {
-		    // Entity Not Found
+			em.getTransaction().begin();
+			Match entity = new Match() ;
+			em.persist(entity);
+			em.getTransaction().commit();
+			try {
+				EntityTest obj = em.getReference(EntityTest.class, entity.getIdentity());
+			   
+			} catch (EntityNotFoundException e) {
+			    // Entity Not Found
+			}
+			
+			em.flush();
+			wb.add( entity.getIdentity());
+		    
+		     javax.persistence.Cache cache = em.getEntityManagerFactory().getCache();
+		    cache.evict(entity.getClass(), entity.getIdentity());
+	    
+		} finally {
+			em.close();
+			em.clear();
 		}
-		em.flush();
-		wb.add( entity.getIdentity());
 	    
-	     javax.persistence.Cache cache = em.getEntityManagerFactory().getCache();
-	    cache.evict(entity.getClass(), entity.getIdentity());
-	    
-	    em.close();
-	    em.clear();
 	}
 	
 
